fe8ce45c8662a054c6e203315ed94799d96359dc
Junior <vitteri81@gmail.com>
2022-09-07 13:54:06 -0300

    modifica funciones

--- index.js
+++ index.js
@@ -7,37 +7,36 @@ document.querySelector("#jugar").onclick = setearJuego;
 
 $cartas.forEach(function ($carta) {
   $carta.onclick = function () {
+    const MILISEGUNDOS_DELAY_JUGADA = 600;
     const color = $carta.id;
     agregarColor($carta, color);
     if ($primeraCartaSeleccionada === null) {
       $primeraCartaSeleccionada = $carta;
-    } else {
-      if ($primeraCartaSeleccionada === $carta) {
-        return;
-      }
-      if ($primeraCartaSeleccionada.id === $carta.id) {
-        bloquearClicksUsuario();
-        setTimeout(() => {
-          eliminarCarta($primeraCartaSeleccionada);
-          eliminarCarta($carta);
-          desbloquearClicksUsuario();
-          verSiGano();
-        }, 600);
-      } else {
-        bloquearClicksUsuario();
-        setTimeout(() => {
-          eliminarColor($carta, color);
-          eliminarColor(
-            $primeraCartaSeleccionada,
-            $primeraCartaSeleccionada.id
-          );
-          desbloquearClicksUsuario();
-        }, 600);
-      }
-      setTimeout(() => {
-        $primeraCartaSeleccionada = null;
-      }, 600);
+      return;
     }
+    if ($primeraCartaSeleccionada === $carta) {
+      return;
+    }
+    if ($primeraCartaSeleccionada.id === $carta.id) {
+      bloquearClicksUsuario();
+      setTimeout(() => {
+        eliminarCarta($primeraCartaSeleccionada);
+        eliminarCarta($carta);
+        desbloquearClicksUsuario();
+        analizarSiHayGanador();
+      }, MILISEGUNDOS_DELAY_JUGADA);
+      return;
+    }
+    bloquearClicksUsuario();
+    setTimeout(() => {
+      eliminarColor($carta, color);
+      eliminarColor($primeraCartaSeleccionada, $primeraCartaSeleccionada.id);
+      desbloquearClicksUsuario();
+    }, MILISEGUNDOS_DELAY_JUGADA);
+
+    setTimeout(() => {
+      $primeraCartaSeleccionada = null;
+    }, MILISEGUNDOS_DELAY_JUGADA);
   };
 });
 
@@ -73,7 +72,7 @@ function mezclarColores(colores) {
   colores.sort((a, b) => 0.5 - Math.random());
 }
 
-function verSiGano() {
+function analizarSiHayGanador() {
   if (document.querySelectorAll(".carta").length === 0) {
     bloquearClicksUsuario();
     alert("Ganaste! para volver a jugar seleccione jugar nuevamente");
